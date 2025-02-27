750752f9aa75365f656e1bb45fe3c2bbe6a1c715
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:47:51 +0100

    feat(shell.h): add new prototype

--- libshell/shell.h
+++ libshell/shell.h
@@ -19,10 +19,22 @@ typedef struct list {
     struct list *next;
 } list_t;
 
+typedef struct flag_s {
+    char *c;
+    int (*fonction)(list_t **head, int reverse);
+}flag_t;
+
 int add(void *data, char **args);
 int del(void *data, char **args);
 int sort(void *data, char **args);
 int disp(void *data, char **args);
 int workshop_shell(void *data);
+int compare_type(list_t *a, list_t *b, int reverse);
+int compare_name(list_t *a, list_t *b, int reverse);
+int compare_id(list_t *a, list_t *b, int reverse);
+int verif_type(list_t **head, int reverse);
+int verif_name(list_t **head, int reverse);
+int verif_id(list_t **head, int reverse);
+void swap_nodes(list_t *a, list_t *b);
 
 #endif /* SHELL_H */
