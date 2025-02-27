4f17ea2f35cd01c4f0eb63fe2da15138ab10b995
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:47:41 +0100

    chore(add): split function add in 2 for coding style

--- src/add.c
+++ src/add.c
@@ -27,30 +27,41 @@ int handle_add(char **args)
     return 0;
 }
 
+list_t *create_element(char *type, char *name, int id)
+{
+    list_t *new = malloc(sizeof(list_t));
+
+    if (!new)
+        return NULL;
+    new->id = id;
+    new->type = my_strdup(type);
+    new->name = my_strdup(name);
+    if (!new->type || !new->name) {
+        free(new->type);
+        free(new->name);
+        free(new);
+        return NULL;
+    }
+    return new;
+}
+
 int add(void *data, char **args)
 {
     list_t **new_data = data;
-    list_t *new;
     static int id_count = 0;
+    list_t *new;
 
     if (handle_add(args) == 84)
         return 84;
     for (int i = 0; args[i]; i += 2) {
-        new = malloc(sizeof(list_t));
-        if (!new)
-            return 84;
-        new->id = id_count;
+        new = create_element(args[i], args[i + 1], id_count);
         id_count++;
-        new->type = my_strdup(args[i]);
-        if (!new->type)
-            return 84;
-        new->name = my_strdup(args[i + 1]);
-        if (!new->name)
+        if (!new)
             return 84;
         new->next = *new_data;
         *new_data = new;
-        mini_printf("%s n°%d - \"%s\" added.\n", (*new_data)->type,
-            (*new_data)->id, (*new_data)->name);
+        mini_printf("%s n°%d - \"%s\" added.\n", new->type, new->id,
+            new->name);
     }
     return 0;
 }
