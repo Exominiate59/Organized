067caec953fb54eb9fafd113080c50d553bcac5b
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:47:16 +0100

    feat(disp): add function that display materials

---
+++ src/disp.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LIL-1-1-organized-antoine.rousselle
+** File description:
+** disp.c
+*/
+
+#include "../libshell/shell.h"
+
+int disp(void *data, char **args)
+{
+    list_t **new_data = data;
+    list_t *temp = *new_data;
+
+    if (args[0] != NULL)
+        return 84;
+    for (; temp; temp = temp->next)
+        mini_printf("%s n°%d - \"%s\"\n", temp->type, temp->id, temp->name);
+    return 0;
+}
