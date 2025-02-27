6deb92c79d3fa83e135d2c657f6da4584935742c
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:47:18 +0100

    feat(orgonized): add principal function that call workshop_shell

---
+++ src/organized.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LIL-1-1-organized-antoine.rousselle
+** File description:
+** organized.c
+*/
+
+#include "../libshell/shell.h"
+
+int main(void)
+{
+    list_t *begin = NULL;
+
+    return workshop_shell(&begin);
+}
